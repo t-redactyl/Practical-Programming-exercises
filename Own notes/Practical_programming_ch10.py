b518248be97a2b3aa2774c2f7efd1c2d3d9ee97a
Jodie Burchell <jodie.burchell@gmail.com>
2015-10-22 13:55:54 +1100

    Completed Chapter 10

---
+++ Own notes/Practical_programming_ch10.py
@@ -0,0 +1,8 @@
+# Practical Programming Chapter 10 notes
+
+# Searching
+counts = [809, 834, 477, 478, 307, 122, 96, 102, 324, 476]
+min(counts)
+low = min(counts)
+min_index = counts.index(low)
+counts.index(min(counts))
